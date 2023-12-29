1f66b7627b23609bc092e23dac6f9b8faac1702f
Leandro <leandro.barreto.lb23@gmail.com>
2023-12-29 09:11:03 -0300

    add ex008 ex009 ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+let velocidade = 78
+
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+if(velocidade > 60) { //condicao simples
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)

1c46a45e4d8b47f07ac9b6e32832c58ce70ba4f1
Leandro <leandro.barreto.lb23@gmail.com>
2024-01-04 12:00:14 -0300

    add pt005 layout and openModal

---
+++ aula14/exercicios/pt005/script.js
@@ -0,0 +1,7 @@
+const btnAdd = document.getElementById('btn-add')
+const modalWrapper = document.getElementById('modal-wrapper')
+
+btnAdd.addEventListener('click', () => {
+  modalWrapper.classList.add('open')
+})
+
